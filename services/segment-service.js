c6b7877f3da4c83676ade31eff081336943ffb26
Gunjan Gupta <gugupta@twilio.com>
2024-12-19 11:31:18 -0600

    Update Segment getUserProfile
    to escape ASCII Characters

--- services/segment-service.js
+++ services/segment-service.js
@@ -165,11 +165,16 @@ const addInteraction = (id, eventName, data) => {
     console.error("Error adding addEvent:", error);
   }
 };
-
 const getUserProfile = async (id) => {
+  //client:+15123485523 -> client%3A%2B15123485523
+  const userId = id.replace(/\+/g, '%2B').replace(/:/g, '%3A');
+  const baseUrl = `https://profiles.segment.com/v1/spaces/${spaceID}/collections/users/profiles/`;
+  const traitsUrl = `${baseUrl}user_id:${userId}/traits`;
+  console.log(traitsUrl);
+
+  // encode base64
   const username = profileToken;
   const password = "";
-  // encode base64
   const credentials = Buffer.from(`${username}:${password}`).toString("base64");
 
   // set headers
@@ -178,22 +183,19 @@ const getUserProfile = async (id) => {
       Authorization: `Basic ${credentials}`,
     },
   };
-
-  console.log("get_profile from segment for id: " + id);
-
   try {
     const response = await axios.get(
-      `https://profiles.segment.com/v1/spaces/${spaceID}/collections/users/profiles/user_id:${id}/traits`,
+      traitsUrl,
       config
     );
-
+    console.log("axios" + response);
     const traits = response.data.traits;
     return traits;
   } catch (e) {
     console.log("get_profile error:", error);
     return "";
   }
-};
+}
 
 //to do - GG
 const updateUserProfile = async (id) => {
